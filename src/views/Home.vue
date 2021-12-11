bd6c690ad9495ce9e8c36aa9a64d366df78cc68f
Hannah Ejimofor <hejimofor141@uniport.edu.ng>
2021-12-11 02:41:04 +0100

    ui setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-wrapper">
-    <div class="flex flex-row gap-10 items-center my-40">
-      <div class="w-1/3">
+    <div class="flex flex-row gap-10 items-center mt-60 mb-40">
+      <div class="w-1/3 relative">
         <div class="display-rack">
           <img class="cat-image" src="../assets/images/shared/desktop/image-headphones.png" />
           <p class="font-bold">HEADPHONES</p>
@@ -75,6 +75,6 @@ export default {
   border-radius: 5px;
   width: 100%;
   /* height: 250px; */
-  padding: 70px 0px;
+  padding: 50px 0px;
 }
 </style>
